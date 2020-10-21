77117142d0ab0a6f8c5974fd14dbe060e4f9b9d5
Enzo Giglio <enzo.giglio@live.fr>
2020-10-21 12:26:48 +0200

    midday 32

---
+++ unity_test_project/Assets/SampleScenes/Scripts/Boid.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    public float zoneRepulsion = 5;
+    public float zoneAlignement = 9;
+    public float zoneAttraction = 50;
+
+    public float forceRepulsion = 15;
+    public float forceAlignement = 3;
+    public float forceAttraction = 20;
+
+    public Vector3 velocity = new Vector3();
+    public float maxSpeed = 20;
+    public float minSpeed = 12;
+
+    public bool drawGizmos = true;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (drawGizmos)
+        {
+            // Répulsion
+
+            Gizmos.color = new Color(1, 0, 0, 1.0f);
+            Gizmos.DrawWireSphere(transform.position, zoneRepulsion);
+
+            // Alignement
+
+            Gizmos.color = new Color(0, 1, 0, 1.0f);
+            Gizmos.DrawWireSphere(transform.position, zoneAlignement);
+
+            // Attraction
+
+            Gizmos.color = new Color(0, 0, 1, 1.0f);
+            Gizmos.DrawWireSphere(transform.position, zoneAttraction);
+        }
+    }
+}
---
+++ unity_test_project/Assets/SampleScenes/Scripts/BoidManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+
+public class BoidManager : MonoBehaviour
+{
+    private static BoidManager instance = null;
+    public static BoidManager sharedInstance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<BoidManager>();
+            }
+            return instance;
+        }
+    }
+
+
+    public Boid prefabBoid;
+    public float nbBoids = 30;
+    public float startSpeed = 1;
+    public float startSpread = 10;
+
+    private List<Boid> boids = new List<Boid>();
+
+    void Start()
+    {
+        for (int i = 0; i < nbBoids; i++)
+        {
+            Boid b = GameObject.Instantiate<Boid>(prefabBoid);
+            Vector3 positionBoid = Random.insideUnitSphere * startSpread;
+            positionBoid.y = Mathf.Abs(positionBoid.y); //Ne pas créer des oiseaux sous 0, on imagine que ce sera le sol.
+            b.transform.position = positionBoid;
+            b.velocity = (positionBoid - transform.position).normalized * startSpeed;
+            b.transform.parent = this.transform;
+            boids.Add(b);
+        }
+    }
+}
